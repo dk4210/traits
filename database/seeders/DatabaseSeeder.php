e98e5c731648160cb5e85a626af6764397fde34a
Dan Kelly <dankelly2006@gmail.com>
2022-08-16 11:09:54 -0400

    Added Trait example

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $faker = Faker::create();
+        $gender = $faker->randomElement(['male', 'female']);
+        foreach (range(1,10) as $index) {
+            DB::table('students')->insert([
+                'name'  => $faker->name($gender),
+                'email' => $faker->email
+            ]);
+        }
+
+        
+    //    \App\Models\User::factory(10)->create();
+
+    //      \App\Models\User::factory()->create([
+    //          'name' => 'Test User',
+    //          'email' => 'test@example.com',
+    //         ]);
     }
 }
